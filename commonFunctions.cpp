8ec3d5f2b328426036508adc44cec53b155a1c69
kamil <kamil.sapek1@gmail.com>
2024-12-13 17:01:40 +0100

    translacja na angielski

---
+++ commonFunctions.cpp
@@ -0,0 +1,32 @@
+//
+// Created by Kamil on 12/9/2024.
+//
+
+#include "commonFunctions.h"
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> createVector(string data, char splitter) {
+    data += splitter;
+    vector<int> vectorToReturn;
+    string test;
+    for (int i = 0; i < data.length(); i++) {
+        if (data.at(i) == splitter) {
+            vectorToReturn.push_back(stoi(test));
+            test = "";
+        } else {
+            test += data.at(i);
+        }
+    }
+    return vectorToReturn;
+}
+
+std::string readVector(std::vector<int> vector, const char splitter) {
+    string stringToReturn;
+    for (int i : vector) {
+        stringToReturn += to_string(i) + splitter;
+    }
+    stringToReturn.pop_back();
+    return stringToReturn;
+}
---
+++ commonFunctions.h
@@ -0,0 +1,16 @@
+
+//
+// Created by Kamil on 12/9/2024.
+//
+
+#ifndef COMMONFUNCTIONS_H
+#define COMMONFUNCTIONS_H
+#include <string>
+#include <vector>
+
+std::vector<int> createVector(std::string data, char splitter);
+std::string readVector(std::vector<int> vector, const char splitter);
+
+
+
+#endif //COCOMMONFUNCTIONS_H
