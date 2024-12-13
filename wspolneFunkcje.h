e2f7330ab6ae02f15d853c64af5eee0ff62a36b5
Kamil <kamil.sapek1@gmail.com>
2024-12-13 12:53:09 +0100

    zmiana na funkcje static w klasie wspolneFunkcje

---
+++ wspolneFunkcje.h
@@ -0,0 +1,15 @@
+//
+// Created by Kamil on 12/9/2024.
+//
+
+#ifndef WPSOLNEFUNKCJE_H
+#define WPSOLNEFUNKCJE_H
+#include <string>
+#include <vector>
+
+static std::vector<int> utworzWektor(std::string dane, char rozdzielnik);
+static std::string odczytajWektor(std::vector<int> wektor, const char rozdzielnik);
+
+
+
+#endif //WPSOLNEFUNKCJE_H
